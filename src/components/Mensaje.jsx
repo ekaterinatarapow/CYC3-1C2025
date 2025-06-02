f009c906705e70fc56797d9895e43c937d5cd8d1
Ekaterina Tarapow <katy.upalermo@gmail.com>
2025-06-02 08:29:04 -0300

    Delete src/components/Mensaje.jsx

--- src/components/Mensaje.jsx
+++
@@ -1,9 +0,0 @@
-import "../App.css";
-
-export default function Mensaje({ texto, final }) {
-    return (
-        <div className={`mensaje ${final ? "final" : "frase"}`}>
-            <p>{texto}</p>
-        </div>
-    );
-}
