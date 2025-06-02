826920656df666eed3257ec08c40468499736832
Ekaterina Tarapow <katy.upalermo@gmail.com>
2025-06-02 08:28:05 -0300

    Delete src/components/Boton.jsx

--- src/components/Boton.jsx
+++
@@ -1,9 +0,0 @@
-import "../App.css";
-
-export default function Boton({ texto, onClick, icono }) {
-    return (
-        <button className="boton" onClick={onClick}>
-            {icono && <span className="icono">{icono}</span>} {texto}
-        </button>
-    );
-}
